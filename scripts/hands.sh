77406b65af6c64fdc30f49049e4a4e097d9a8844
Alberto Romay <romay@sim.informatik.tu-darmstadt.de>
2015-04-03 19:08:14 +0200

    Added script to set vt hands

---
+++ scripts/hands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export THOR_ROBOT_TYPE="thor_mang_hands"
+export THOR_LEFT_HAND_TYPE="l_vt_hand"
+export THOR_RIGHT_HAND_TYPE="r_vt_hand"
+
+echo "  Robot type      = "$THOR_ROBOT_TYPE
+echo "  Left hand type  = "$THOR_LEFT_HAND_TYPE
+echo "  Right hand type = "$THOR_RIGHT_HAND_TYPE
