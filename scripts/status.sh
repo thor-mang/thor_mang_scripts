2e9e11f9af4593deef5ab0618f2d77674edc063e
Alberto Romay <romay@sim.informatik.tu-darmstadt.de>
2015-04-03 17:45:31 +0200

    Added colorized output

--- scripts/status.sh
+++ scripts/status.sh
@@ -1,36 +1,94 @@
 #!/bin/bash
 
-git_status () {
-  echo "> in $PWD ..."
-  git status | grep "On branch"
-  git status | grep "modified"
-  git status | grep "branch is ahead"
-  git status | grep "Untracked files"
+function blueEcho()
+{
+  echo -ne '\e[0;34m'
+  echo $1
+  echo -ne '\e[0m'
 }
 
-echo ">>> Looking for locally modified files in install ..."
-echo
-cd ${THOR_ROOT}
-git_status
-echo
+function findCanonicalBranch()
+{
+  dir=$1
+  rosinstalldir=$2
 
-if [ -d $THOR_ROOT/rosinstall/optional/custom/.git ]; then
-    echo ">>> Looking for locally modified custom rosinstalls"
-    echo
-    cd $THOR_ROOT/rosinstall/optional/custom
-    git_status
-    echo
-fi
-
-echo ">>> Looking for locally modified files in ${ROS_WORKSPACE} ..."
-echo
-for d in `find ${ROS_WORKSPACE} -mindepth 1 -type d`;
-do
-  cd $d
-  if [ -d "$d/.git" ]; then
-    git_status
-    echo
+  if [ -e "$rosinstalldir/.rosinstall" ]
+  then
+    name=$(basename $dir)
+    desiredBranch=$(perl -0 -ne 'print qq($1\n) if /\b'$name'\b.*\n.*version: ([^}]+)/mg' ${THOR_ROOT}/.rosinstall)
   fi
+
+  echo $desiredBranch
+}
+
+function displayStatus()
+{
+  old_d=`pwd`
+  dir=$1
+  desiredBranch=$2
+
+  cd $dir
+  if [ -z "$desiredBranch" ]
+  then
+    desiredBranch=$(git log --pretty='%d' -1 remotes/origin/HEAD | perl -ne 'm#(?<=origin/)([^,]*)# && print "$1\n"')
+  fi
+
+  if [ -e "$dir/.git" ]
+  then
+    if [ "$(git rev-parse --abbrev-ref HEAD)" != "$desiredBranch" ] \
+       || [ -n "$(git status --porcelain)" ] \
+       || [ -n "$(git status | grep -P 'branch is (ahead|behind)')" ]
+    then
+      echo "$PWD :"
+      if [ "$(git rev-parse --abbrev-ref HEAD)" != "$desiredBranch" ]
+      then
+        git status | grep "On branch" | perl -pe "chomp"
+        echo -e " (should be on branch $desiredBranch)"
+      fi
+      git status | grep -P 'branch is (ahead|behind)'
+      git status | grep "modified"
+      if [ -n "$(git status | grep 'Untracked files')" ]
+      then
+        git status --porcelain | grep '??' | sed -r 's/^.{3}//' \
+        | xargs -I file echo -e '\tuntracked:  '"file"
+      fi
+      echo
+    fi
+  elif [ -e "$dir/.hg" ]; then
+    if [ "$(hg branch)" != "$desiredBranch" ] \
+       || [ -n "$(hg status)" ]
+    then
+      echo "$PWD :"
+      echo "On hg branch `hg branch`"
+      hg status
+      hg incoming | grep "changes"
+      echo
+    fi
+  #else
+    #echo "$PWD is not a repository!"
+    #echo
+  fi
+  cd $old_d
+}
+
+cd ${THOR_ROOT}
+blueEcho "Looking for changes in $PWD ..."
+displayStatus $PWD
+
+
+catkin_src=${ROS_WORKSPACE}
+blueEcho "Looking for changes in ${catkin_src} ..."
+for d in `find  ${catkin_src} -mindepth 1 -maxdepth 1 -type d`;
+do
+  branch=$(findCanonicalBranch $d ${catkin_src}/..)
+  displayStatus $d $branch
 done
 
-echo "Done!"
+
+if [ -d $THOR_ROOT/rosinstall/optional/custom/.git ]; then
+
+    cd $THOR_ROOT/rosinstall/optional/custom
+    blueEcho "Looking for changes in $PWD ..."
+    displayStatus $PWD
+fi
+
