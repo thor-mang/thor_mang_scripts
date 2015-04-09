637d7f45eface265b43a2f3239184981f1f0eca2
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2015-04-09 00:31:12 -0400

    added THOR-MANG libs to be compiled

--- scripts/make_externals.sh
+++ scripts/make_externals.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# install sbpl by source
+# build and install sbpl by source
+echo
+echo ">>> Building SPBL"
 cd $THOR_ROOT/src/external/sbpl
-if [ ! -d "build" ]; then
-    mkdir build
-fi
+mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 sudo make install
+cd $THOR_ROOT
+
+# build and install thor mang libs
+echo
+echo ">>> Building THOR-MANG libs"
+cd $THOR_ROOT/src/thor_mang/thor_mang_libs
+make
 cd $THOR_ROOT
