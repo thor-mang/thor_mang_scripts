a71356fc6275f418bb70e48c045ef75643e76ed3
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2015-04-09 01:46:44 -0400

    added clean script for externals

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. $THOR_SCRIPTS/clean_externals.sh
+
 cd $THOR_ROOT
 rm -rf $THOR_ROOT/build
 rm -rf $THOR_ROOT/devel
---
+++ scripts/clean_externals.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# build and install sbpl by source
+echo
+echo ">>> Cleaning SPBL"
+cd $THOR_ROOT/src/external/sbpl
+rm -rf build
+cd $THOR_ROOT
+
+# build and install thor mang libs
+echo
+echo ">>> Cleaning THOR-MANG libs"
+cd $THOR_ROOT/src/thor_mang/thor_mang_libs
+rm -rf build lib
+cd $THOR_ROOT
