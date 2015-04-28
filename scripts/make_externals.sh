beeca8f0fa382e230005dd89eb9c31b5a7ee6a7c
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2015-04-28 17:46:26 +0200

    fixed make_externals script

--- scripts/make_externals.sh
+++ scripts/make_externals.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# build and install sbpl by source
+echo
+echo ">>> Building SPBL"
+cd $THOR_ROOT/src/external/sbpl
+mkdir -p build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+sudo make install
+cd $THOR_ROOT
+
 # build and install thor mang libs
 echo
 echo ">>> Building THOR-MANG libs"
 cd $THOR_ROOT/src/thor_mang/thor_mang_libs
 make
 cd $THOR_ROOT
+
