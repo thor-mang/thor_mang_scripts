9f0d1c3e8dcd6f0553b21297955003e0f558df9e
Christian Steiger <christian.steiger@cst-projects.de>
2015-05-17 10:59:41 -0400

    Extend update.sh functionality.
    * The script pulls now automatically from the torc repositories without
      needing username and passwort.
    
    * Some cleanup.
    
    * Added a "--no-merge" option for future commits.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,52 +1,77 @@
 #!/bin/bash
 
-echo
+set -e -u
 
-# Takes us to scripts which is inside workspace, so that's good enough
-# for wstool to work
-echo ">>> Pulling install folder in $THOR_SCRIPTS"
-cd $THOR_SCRIPTS
-git pull
+option=${1:-none}
 
-# Remove obsolete stuff using wstool
+# ---------------------------------------------------------------------------
+
+# Pull custom repos
+repos=" \
+$THOR_ROOT \
+$THOR_ROOT/rosinstall/optional/custom \
+$THOR_SCRIPTS/custom \
+"
+
+for repo in $repos
+do
+	echo "Updating Repository in $repo"
+	cd $repo
+	git pull
+	echo
+done
+
+# ---------------------------------------------------------------------------
+
+echo "Remove obsolete packages..."
 $THOR_SCRIPTS/rm_obsolete_packages.sh
-
-echo ">>> Pulling install folder in $THOR_ROOT"
-cd $THOR_ROOT
-git pull
 echo
 
 #echo ">>> Checking package updates"
 #./rosinstall/install_scripts/install_package_dependencies.sh
 #echo
 
-if [ -d $THOR_ROOT/rosinstall/optional/custom/.git ]; then
-    echo ">>> Pulling custom rosinstalls"
-    cd $THOR_ROOT/rosinstall/optional/custom
-    git pull
-    echo
+# ---------------------------------------------------------------------------
+
+# Merge updates
+if [ ! "$option" = "--no-merge" ]
+then
+	cd $THOR_ROOT
+	echo ">>> Merging rosinstall files"
+	for file in $THOR_ROOT/rosinstall/*.rosinstall; do
+		filename=$(basename ${file%.*})
+		if [ -n "${THOR_MANG_NO_SIM:-}" ] && [ $filename == "thor_mang_simulation" ]
+		then
+			continue;
+		else
+			echo "Merging to workspace: '$filename'.rosinstall"
+			wstool merge $file -y
+		fi
+	done
+	echo
 fi
 
-if [ -d $THOR_SCRIPTS/custom/.git ]; then
-    echo ">>> Pulling custom scripts"
-    cd $THOR_SCRIPTS/custom
-    git pull
-    echo
-fi
+# ---------------------------------------------------------------------------
 
-cd $THOR_ROOT
-echo ">>> Merging rosinstall files"
-for file in $THOR_ROOT/rosinstall/*.rosinstall; do
-    filename=$(basename ${file%.*})
-    if [ -n "$THOR_MANG_NO_SIM" ] && [ $filename == "thor_mang_simulation" ]; then
-        continue;
-    else
-        echo "Merging to workspace: '$filename'.rosinstall"
-        wstool merge $file -y
-    fi
-done
-echo
+# This is an user account with guest access only
+TORC_USER="stefan.kohlbrecher@gmail.com" # Don't use anything that can push!
+TORC_PASS="VqK6NXRL"
 
-echo ">>> Updating catkin workspace"
+# Create build folder if neccessary
+if [ ! -d "$THOR_ROOT/build" ]; then mkdir -v -p $THOR_ROOT/build; fi
+
+# Create expect file and run wstool stuff
+cat > $THOR_ROOT/build/thor-update_expect <<EOF
+spawn wstool update
+expect "Username for 'https://external.torcrobotics.com'"
+send "$TORC_USER\r\n"
+expect "Password for 'https://$TORC_USER@external.torcrobotics.com'"
+send "$TORC_PASS\r\n"
+interact
+EOF
+
+# Run expect file
 cd $THOR_ROOT/src
-wstool update
+expect $THOR_ROOT/build/thor-update_expect
+
+
