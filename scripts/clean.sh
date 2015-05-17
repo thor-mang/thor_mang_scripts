38d160f2283cd73465ee71cd79558110b4776b07
Christian Rose <christian.j.rose@gmx.de>
2015-05-16 21:09:12 -0400

    Fixed the problem that thor workspace is broken when you use thor clean and then close the terminal. Now the setup.bash and correspondigs will not be deleted.

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,4 +4,15 @@
 
 cd $THOR_ROOT
 rm -rf $THOR_ROOT/build
-rm -rf $THOR_ROOT/devel
+
+#rm -rf $THOR_ROOT/devel
+# Remove everything in devel but keep setup.bash and the corresonding things so that the source in .bashrc still works
+rm -rf $THOR_ROOT/devel/bin
+rm -rf $THOR_ROOT/devel/etc
+rm -rf $THOR_ROOT/devel/include
+rm -rf $THOR_ROOT/devel/lib
+rm -rf $THOR_ROOT/devel/share
+rm $THOR_ROOT/devel/.catkin
+rm $THOR_ROOT/devel/.rosinstall
+
+
