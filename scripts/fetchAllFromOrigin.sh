277e823a06894712bcd3e1494305ac36a86442a7
Achim Stein <steinachim@gmx.de>
2015-05-17 02:33:52 +0200

    added fetch script

--- scripts/fetchAllFromOrigin.sh
+++ scripts/fetchAllFromOrigin.sh
@@ -12,7 +12,7 @@ import argparse
 LOCAL_REPO_DIRECTORY = '/home/hector/thor/'
 ROSBUILD_DIRECTORY = '/home/hector/thor/rosinstall/'
 
-def parse(reset):
+def parse():
     pattern  = ROSBUILD_DIRECTORY+"*.rosinstall"
     for index, script in enumerate(sorted(glob.glob(pattern))):
         with open (script, "r") as myfile:
@@ -25,14 +25,7 @@ def parse(reset):
             
             if(os.path.isdir(local_folder)):                          
                 print "Now processing "+local_folder+" folder...."
-                if(reset):
-                    os.system("cd "+local_folder+";git config --bool core.bare false")
-                else:
-                    os.system("cd "+local_folder+";git config --bool core.bare true");
-                #os.system("cd " + local_folder + "; git checkout master; git branch -d NO_COMMIT_BRANCH; git push origin :NO_COMMIT_BRANCH");
+                os.system("cd "+local_folder+";git fetch origin +refs/heads/*:refs/heads/* --prune")
                 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description = "Setup machine to be able to push/pull to machine on local network", epilog=__doc__)
-    parser.add_argument("-r", "--reset", action="store_true", help="reconfigure computers if local commits have already been setup")
-    args = parser.parse_args()
-    parse(args.reset)
+    parse()
