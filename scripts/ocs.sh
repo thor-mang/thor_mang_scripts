7fd0c7c261f3c895038b44ad9ac29e5b9e082e4b
Martin Oehler <martin.sven.oehler@gmail.com>
2015-05-25 08:58:58 -0400

    ocs script now takes arguments for different pc setups

--- scripts/ocs.sh
+++ scripts/ocs.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-
-roslaunch thor_mang_ocs thor_mang_ocs.launch
+command=$1
+shift
+if [ $command == "primary" ]; then
+  echo "Starting primary ocs"
+  roslaunch thor_mang_ocs primary_ocs.launch "$@"
+elif [ $command == "perception" ]; then
+  echo "Starting perception ocs"
+  roslaunch thor_mang_ocs perception_ocs.launch  
+elif [ $command == "behavior" ]; then
+  echo "Starting behavior ocs"
+  roslaunch thor_mang_ocs behavior_ocs.launch
+elif [ $command == "driving" ]; then
+  echo "Starting driving widget"
+  roslaunch thor_mang_driving_widget driving_widget.launch
+else
+  roslaunch thor_mang_ocs thor_mang_ocs.launch
+fi
