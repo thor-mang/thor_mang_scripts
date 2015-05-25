a0fd34cae10fe976e304c4831b76045929eae451
Hector OCS Perception <hector-drc@gmx.com>
2015-05-25 15:16:25 -0400

    removed incorrect ros master settigns

--- scripts/ocs.sh
+++ scripts/ocs.sh
@@ -4,14 +4,19 @@ shift
 if [ $command == "primary" ]; then
   echo "Starting primary ocs"
   roslaunch thor_mang_ocs primary_ocs.launch "$@"
+
 elif [ $command == "perception" ]; then
   echo "Starting perception ocs"
+  #thor master ocs-primary
+  #thor master thor-motion
   roslaunch thor_mang_ocs perception_ocs.launch  
-  thor master ocs-primary
+    
 elif [ $command == "behavior" ]; then
   echo "Starting behavior ocs"
+  #thor master ocs-primary
+  #thor master thor-motion
   roslaunch thor_mang_ocs behavior_ocs.launch
-  thor master ocs-primary
+  
 elif [ $command == "driving" ]; then
   echo "Starting driving widget"
   roslaunch thor_mang_driving_widget driving_widget.launch
