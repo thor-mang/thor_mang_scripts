154292ae8a96ee3e6b515c14d01b893b435a402a
Hector OCS Perception <hector-drc@gmx.com>
2015-05-27 11:29:12 -0400

    fixed ocs script

--- scripts/ocs.sh
+++ scripts/ocs.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
-command=$1
-shift
-if [ $command == "primary" ]; then
-  echo "Starting primary ocs"
-  roslaunch thor_mang_ocs primary_ocs.launch "$@"
-
-elif [ $command == "perception" ]; then
-  echo "Starting perception ocs"
-  #thor master ocs-primary
-  #thor master thor-motion
-  roslaunch thor_mang_ocs perception_ocs.launch  
-    
-elif [ $command == "behavior" ]; then
-  echo "Starting behavior ocs"
-  #thor master ocs-primary
-  #thor master thor-motion
-  roslaunch thor_mang_ocs behavior_ocs.launch
-  
-elif [ $command == "driving" ]; then
-  echo "Starting driving widget"
-  roslaunch thor_mang_driving_widget driving_widget.launch
-else
+if [ "$#" -lt 1 ]; then
+  echo "Launching complete ocs."
   roslaunch thor_mang_ocs thor_mang_ocs.launch
+else
+  command=$1
+  shift
+  if [ $command == "primary" ]; then
+    echo "Starting primary ocs"
+    roslaunch thor_mang_ocs primary_ocs.launch "$@"
+
+  elif [ $command == "perception" ]; then
+    echo "Starting perception ocs"
+    #thor master ocs-primary
+    #thor master thor-motion
+    roslaunch thor_mang_ocs perception_ocs.launch "$@"  
+      
+  elif [ $command == "behavior" ]; then
+    echo "Starting behavior ocs"
+    #thor master ocs-primary
+    #thor master thor-motion
+    roslaunch thor_mang_ocs behavior_ocs.launch "$@"
+    
+  elif [ $command == "driving" ]; then
+    echo "Starting driving widget"
+    roslaunch thor_mang_driving_widget driving_widget.launch "$@"
+  else
+    echo "Unknown command, launching complete ocs."
+    roslaunch thor_mang_ocs thor_mang_ocs.launch
+  fi
 fi
