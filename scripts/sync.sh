22be5e56d7f7203a72c8b79f83a2f907a8422f0c
Martin Oehler <martin.sven.oehler@gmail.com>
2015-08-18 19:28:09 +0200

    update to sync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 sudo /etc/init.d/chrony stop
-sudo ntpdate thor-ocs-primary
-sudo /etc/init.d/chrony start
+if [ "$#" -lt 1 ]; then
+  sudo ntpdate thor-motion
+else
+  sudo ntpdate $1
+fi
+  sudo /etc/init.d/chrony start
+  
