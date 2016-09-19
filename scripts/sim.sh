d09f8f0a2bf5d174da59fc85790e07626099123e
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2016-09-19 09:28:46 +0200

    adding 'onboard' option for the sim script starting onboard software as well

--- scripts/sim.sh
+++ scripts/sim.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-roslaunch thor_mang_gazebo start_all.launch "$@"
+if [[ "$1" = "onboard" ]]; then
+    shift
+    roslaunch thor_mang_gazebo start_onboard_all.launch "$@"
+else
+    roslaunch thor_mang_gazebo start_all.launch "$@"
+fi
