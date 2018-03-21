e4f2813ff1761ee0afc6a6c005b9adc7b2de9d7a
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2018-03-21 16:40:18 +0100

    updated robotis merge script

--- scripts/merge_robotis.sh
+++ scripts/merge_robotis.sh
@@ -63,16 +63,17 @@ echo ">>> Syncing master branches <<<"
 echo "-------------------------------"
 echo
 
-merge dynamixel_sdk     master  https://github.com/ROBOTIS-GIT/DynamixelSDK.git             master
-merge common            master  https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-Common.git  master
-merge math              master  https://github.com/ROBOTIS-GIT/ROBOTIS-Math.git             master
-merge thormang_msgs     master  https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-msgs.git    master
-merge framework_msgs    master  https://github.com/ROBOTIS-GIT/ROBOTIS-Framework-msgs.git   master
-merge framework         master  https://github.com/ROBOTIS-GIT/ROBOTIS-Framework.git        master
-merge tools             master  https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-Tools.git   master
-merge mpc               master  https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-MPC.git     master
-merge opc               master  https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-OPC.git     master
-merge ppc               master  https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-PPC.git     master
+merge dynamixel_sdk           master  https://github.com/ROBOTIS-GIT/DynamixelSDK.git             master
+merge common                  master  https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-Common.git  master
+merge math                    master  https://github.com/ROBOTIS-GIT/ROBOTIS-Math.git             master
+merge thormang_msgs           master  https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-msgs.git    master
+merge framework_msgs          master  https://github.com/ROBOTIS-GIT/ROBOTIS-Framework-msgs.git   master
+merge framework               master  https://github.com/ROBOTIS-GIT/ROBOTIS-Framework.git        master
+merge tools                   master  https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-Tools.git   master
+merge mpc                     master  https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-MPC.git     master
+merge opc                     master  https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-OPC.git     master
+merge ppc                     master  https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-PPC.git     master
+merge rh_p12_rn_description   master  https://github.com/ROBOTIS-GIT/RH-P12-RN.git                master
 
 echo
 echo "--------------------------------"
@@ -80,13 +81,14 @@ echo ">>> Syncing robotis branches <<<"
 echo "--------------------------------"
 echo
 
-merge dynamixel_sdk     robotis https://github.com/ROBOTIS-GIT/DynamixelSDK.git             kinetic-devel
-merge common            robotis https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-Common.git  kinetic-devel
-merge math              robotis https://github.com/ROBOTIS-GIT/ROBOTIS-Math.git             kinetic-devel
-merge thormang_msgs     robotis https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-msgs.git    kinetic-devel
-merge framework_msgs    robotis https://github.com/ROBOTIS-GIT/ROBOTIS-Framework-msgs.git   kinetic-devel
-merge framework         robotis https://github.com/ROBOTIS-GIT/ROBOTIS-Framework.git        kinetic-devel
-merge tools             robotis https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-Tools.git   kinetic-devel
-merge mpc               robotis https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-MPC.git     kinetic-devel
-merge opc               robotis https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-OPC.git     kinetic-devel
-merge ppc               robotis https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-PPC.git     kinetic-devel
+merge dynamixel_sdk             robotis https://github.com/ROBOTIS-GIT/DynamixelSDK.git             kinetic-devel
+merge common                    robotis https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-Common.git  kinetic-devel
+merge math                      robotis https://github.com/ROBOTIS-GIT/ROBOTIS-Math.git             kinetic-devel
+merge thormang_msgs             robotis https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-msgs.git    kinetic-devel
+merge framework_msgs            robotis https://github.com/ROBOTIS-GIT/ROBOTIS-Framework-msgs.git   kinetic-devel
+merge framework                 robotis https://github.com/ROBOTIS-GIT/ROBOTIS-Framework.git        kinetic-devel
+merge tools                     robotis https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-Tools.git   master
+merge mpc                       robotis https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-MPC.git     kinetic-devel # master
+merge opc                       robotis https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-OPC.git     master
+merge ppc                       robotis https://github.com/ROBOTIS-GIT/ROBOTIS-THORMANG-PPC.git     kinetic-devel
+merge rh_p12_rn_description     robotis https://github.com/ROBOTIS-GIT/RH-P12-RN.git                kinetic-devel
